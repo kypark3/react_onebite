65e5202a44c5c879bf37e3321e96f27021bc7405
ky <luminus.d3@gmail.com>
2024-07-27 23:41:38 +0900

    Register 컴포넌트 간단하게 만들기

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -7,11 +7,59 @@ import { useState } from "react";
 // 4. 자기소개
 
 const Register = () => {
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    nation: "",
+    bio: "",
+  });
+
+  const onChange = (e) => {
+    console.log(e.target.name);
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  /* 복잡한 과정을 간단하게함.
+
+  const onChangeName = (e) => {
+    console.log(e.target.name);
+    setInput({
+      ...input,
+      name: e.target.value,
+    });
+    
+  };
+
+  const onChangeBirth = (e) => {
+    setInput({
+      ...input,
+      birth: e.target.value,
+    });
+  };
+
+  const onChangeNation = (e) => {
+    setInput({
+      ...input,
+      nation: e.target.value,
+    });
+  };
+
+  const onChangeBio = (e) => {
+    setInput({
+      ...input,
+      bio: e.target.value,
+    });
+  };
+
+   
   const [name, setName] = useState("이름");
   const [birth, setBirth] = useState("1991-01-01");
   const [nation, setNation] = useState("국가");
   const [bio, setBio] = useState("자기소개");
-
+    
   const onChangeName = (e) => {
     setName(e.target.value);
   };
@@ -27,31 +75,42 @@ const Register = () => {
   const onChangeBio = (e) => {
     setBio(e.target.value);
   };
-
+*/
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} placeholder={"이름"} />
-        {name}
-      </div>
-      <div>
-        <input value={birth} onChange={onChangeBirth} type="date" />
-        {birth}
+        <input
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
+        {input.name}
       </div>
 
       <div>
-        <select value={nation} onChange={onChangeNation}>
+        <input
+          name="birth"
+          value={input.birth}
+          onChange={onChange}
+          type="date"
+        />
+        {input.birth}
+      </div>
+
+      <div>
+        <select name="nation" value={input.nation} onChange={onChange}>
           <option value={""}></option>
           <option value={"kr"}>한국</option>
           <option value={"us"}>미국</option>
           <option value={"uk"}>영국</option>
         </select>
-        {nation}
+        {input.nation}
       </div>
 
       <div>
-        <textarea value={bio} onChange={onChangeBio} />
-        {bio}
+        <textarea name="bio" value={input.bio} onChange={onChange} />
+        {input.bio}
       </div>
     </div>
   );
